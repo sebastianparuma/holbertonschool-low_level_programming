73e5d078aac3b00d046b53a2af93c8030471fd8f
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-09-17 04:38:26 +0000

    Fixing comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point.
  *
  * Return: Always 0.
  */
