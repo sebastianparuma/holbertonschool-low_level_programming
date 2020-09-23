b4ec210367918b2f134ce75d2d6801fd95cbe4a0
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-09-23 01:35:58 +0000

    second commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _islower - Checks for lowercase character.
+ * @c: Character.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
