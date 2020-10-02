2a23261a54990519b0624b1ab46d8f63aa2e6b98
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-10-02 03:56:58 +0000

    Fixing first task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, j = 0;
 
-	while (dest[index++])
-		dest_len++;
+	for (; dest[i]; i++);
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	while (*src)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	i++;
+	dest[i] = '\0';
 	return (dest);
 }
