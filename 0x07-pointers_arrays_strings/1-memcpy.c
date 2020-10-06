99f826c4f7839f2c215bc26eebed12e89abebd47
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-10-06 04:42:16 +0000

    Fixing data types and prototype of task number one

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,17 +2,17 @@
 
 /**
  * _memcpy - Copies memory area
- * @dest: Pointer
- * @src: Source buffer
- * @n: Number of bytes
+ * @dest: Pointer.
+ * @src: Source buffer.
+ * @n: Number of bytes.
  *
  * Return: Pointer to @dest.
  */
-void *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	unsigned char *source = src;
+	char *destination = dest;
+	char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
