44e384b5c649db1a2014ddf82f10e4f233cea7ae
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-11-10 04:36:01 +0000

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdandard output.
+ * @filename: Pointer.
+ * @letters: Number of letters it should read and print.
+ *
+ * Return: The actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
